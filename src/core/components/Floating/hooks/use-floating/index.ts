674ed759a85e94bfb18f739f4fb3f026c44c810c
Andrew Lenhart <andrew.code21@gmail.com>
2024-05-20 22:16:44 -0400

    feat(core): initial floating hooks and functionality

---
+++ src/core/components/Floating/hooks/use-floating/index.ts
@@ -0,0 +1,68 @@
+import { useDidUpdate } from '@/hooks';
+import { useFloatingUpdate } from '../use-floating-update';
+import { UseFloatingOptions } from '../../Floating.types';
+import { getFloatingMiddleware } from '../../utils/get-floating-middleware';
+import { useFloating as useFloatingPayload, UseFloatingReturn } from '@floating-ui/react';
+import React from 'react';
+
+const useFloating = <E extends Element = Element>(opts: UseFloatingOptions) => {
+  const onClose = React.useCallback(
+    (open: boolean) => {
+      if (open) {
+        opts.onChange?.(false);
+        opts.onClose?.();
+      }
+    },
+    [opts.onChange, opts.onClose]
+  );
+
+  const onChange = React.useCallback(
+    (open: boolean) => {
+      if (!open) {
+        opts.onOpen?.();
+        opts.onChange(true);
+      }
+      if (open) {
+        opts.onChange?.(false);
+        opts.onClose?.();
+      }
+    },
+    [opts.onChange, opts.onClose, opts.onOpen]
+  );
+
+  const payload: UseFloatingReturn<E> = useFloatingPayload({
+    strategy: opts.strategy,
+    placement: opts.placement,
+    middleware: getFloatingMiddleware(opts, () => payload),
+  });
+
+  useFloatingUpdate({
+    open: opts.open,
+    floating: payload,
+    placement: opts.placement,
+    onPlacementChange: opts.onPlacementChange,
+    placementDependencies: opts.placementDependencies,
+  });
+
+  useDidUpdate(() => {
+    opts.onPlacementChange?.(payload.placement);
+  }, [payload.placement]);
+
+  useDidUpdate(() => {
+    if (!opts.open) {
+      opts.onClose?.();
+    }
+    if (opts.open) {
+      opts.onOpen?.();
+    }
+  }, [opts.open]);
+
+  return {
+    open: opts.open,
+    payload,
+    onChange,
+    onClose,
+  };
+};
+
+export { useFloating };
--- src/core/components/Floating/utils/index.ts
+++ src/core/components/Floating/utils/index.ts
@@ -1,4 +1,2 @@
 export * from './get-floating-middleware';
 export * from './get-floating-placement';
-export * from './use-floating-payload';
-export * from './use-floating-update';
